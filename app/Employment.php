f09113d79125355cbf3ed958e767e289e3b348e2
vagrant <vagrant@homestead>
2018-06-01 14:47:14 +0000

    revert back. must tell model table name. didnt use laravel naming standards

--- app/Employment.php
+++ app/Employment.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Employment extends Model
 {
     //
-    //protected $table = 'employment';
+    protected $table = 'employment';
 }
--- app/Skill.php
+++ app/Skill.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //protected $table = 'skill';
+    protected $table = 'skill';
 }
